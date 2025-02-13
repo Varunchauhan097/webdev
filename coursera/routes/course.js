75364f1a3996b5011798088f38eebe4c635e1037
Varun chauhan <115783538+Varunchauhan097@users.noreply.github.com>
2025-02-13 09:55:37 +0530

    Update course.js
    "update"

--- coursera/routes/course.js
+++ coursera/routes/course.js
@@ -4,7 +4,9 @@ const {courseModel, purchaseModel} = require("../db")
 
 
 courseRouter.post("purchase", function(req, res){
-
+    const userId = req.body.userId,
+    const courseId = req.body.courseId
+    
 })
 
 courseRouter.get("preview", function(req, res){
