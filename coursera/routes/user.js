fa8b7a9167da5faa8967c99db760027d2f6afdb8
Varun chauhan <115783538+Varunchauhan097@users.noreply.github.com>
2025-02-13 10:36:44 +0530

    Update user.js
    "update the purchases endpoint"

--- coursera/routes/user.js
+++ coursera/routes/user.js
@@ -1,6 +1,6 @@
 const {Router} = require("express")
 const userRouter = Router();
-const {userModel } = require("../db")
+const {userModel, purchaseModel } = require("../db")
 
 userRouter.post("/signin", function(req, res){
 
@@ -9,6 +9,21 @@ userRouter.post("/signin", function(req, res){
 userRouter.post("/signup", function(req, res){
 
 }) 
+
+userRouter.get("/purchases", userMiddleware , async function(req, res){
+    const userId = req.userId;
+    const purchases = await purchaseModel.find({
+        userId
+    })
+    const courseData = await courseModel.find({
+        _id: { $in: purchases.map(x => x.courseId) }
+    })
+    
+    res.json({
+        purchases,
+        coursesData
+    }) 
+})
 
 module.exports = {
     userRouter
